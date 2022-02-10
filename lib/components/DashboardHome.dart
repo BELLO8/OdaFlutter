44fa79fef3bf6fa4a7e4060e7e0cf2b7afc285ac
BELLO KADER ADETOUDJI <bellokader8@gmail.com>
2022-02-10 15:49:40 +0000

    Dashboard

--- lib/components/DashboardHome.dart
+++ lib/components/DashboardHome.dart
@@ -10,6 +10,36 @@ class DashboardHome extends StatefulWidget {
 class _DashboardHomeState extends State<DashboardHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 52, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text("DashBoard",
+                  style: TextStyle(
+                    color: Color(0xFF071A58),
+                    fontSize: 33,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                  )),
+            ),
+            Container(
+                width: 400,
+                height: 200,
+                margin: EdgeInsets.symmetric(vertical: 22),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(23),
+                  color: Color(0xFF0F043B),
+                ),
+                child: Column(
+                  children: [],
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
